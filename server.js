29737a3c032b9635ac19f21a34253e2ba995b62b
TinhCm <66770122+TinhCm@users.noreply.github.com>
2022-02-13 14:38:39 +0700

    f

--- server.js
+++ server.js
@@ -95,27 +95,34 @@ server.use(/^(?!\/auth).*$/, (req, res, next) => {
     if (req.headers.origin === 'https://o3tea.tk/') {
         next();
     } else {
-        if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+        if (req.headers.origin === 'https://o3tea-admin.tk/') {
+            if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+                const status = 401
+                const message = 'Error in authorization format'
+                res.status(status).json({ status, message })
+                return
+            }
+            try {
+                let verifyTokenResult;
+                verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+                if (verifyTokenResult instanceof Error) {
+                    const status = 401
+                    const message = 'Access token not provided'
+                    res.status(status).json({ status, message })
+                    return
+                }
+                next()
+            } catch (err) {
+                const status = 401
+                const message = 'Error access_token is revoked'
+                res.status(status).json({ status, message })
+            }
+        } else {
             const status = 401
             const message = 'Error in authorization format'
             res.status(status).json({ status, message })
             return
-        }
-        try {
-            let verifyTokenResult;
-            verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
-
-            if (verifyTokenResult instanceof Error) {
-                const status = 401
-                const message = 'Access token not provided'
-                res.status(status).json({ status, message })
-                return
-            }
-            next()
-        } catch (err) {
-            const status = 401
-            const message = 'Error access_token is revoked'
-            res.status(status).json({ status, message })
         }
     }
 })
